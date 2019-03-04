4dfa757aadf1a4b9cfc02698c8b4950a09b104a0
VictorGastelum <gastelum.victor.euro@gmail.com>
2019-03-04 07:54:18 -0600

    a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
         public static void main(String[] args) {
@@ -11,6 +14,15 @@ public class Main {
             myArray[2] = new Hovercraft("Sue's Hover-craft",6,80,8,4.1);
             myArray[3] = new Frigate("Money Waster", 2,85,9.10);
             myArray[4] = new PoliceCar("Dodge charger", 4, 152, 4);
+
+            List<Vehicle> myList = new LinkedList<Vehicle>();
+            myList.add(new Jeep("Fred's Jeep",4,120,4));
+            myList.add(new Jeep("Frank's Jeep", 2,140, 4));
+            myList.add(new Hovercraft("Sue's Hover-craft",6,80,8,4.1));
+            myList.add(new Frigate("Money Waster", 2,85,9.10));
+            myList.add(new PoliceCar("Dodge charger", 4, 152, 4));
+
+
 
             for (int i=0; i<myArray.length; i++) {
 
@@ -24,6 +36,21 @@ public class Main {
                 if (myArray[i] instanceof Emergency) {
                     Emergency xv = (Emergency)myArray[i];
                     xv.soundSiren();
+                }
+            }
+
+            for (int i = 0; i < myList.size(); i++) {
+
+                System.out.println(myList.get(i).getName());
+
+                if (myList.get(i) instanceof LandVehicle) {
+                    LandVehicle lv = (LandVehicle) myList.get(i);
+                    lv.drive();
+                }
+
+                if (myList.get(i) instanceof Emergency) {
+                    Emergency ie = (Emergency) myList.get(i);
+                    ie.soundSiren();
                 }
             }
 
